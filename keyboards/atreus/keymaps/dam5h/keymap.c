8d119af65092858517f2728e2430293408e6b945
Dave McDaniel <mcdanieldave@gmail.com>
2024-01-26 19:48:44 -0500

    add swoop keymap for atreus

---
+++ keyboards/atreus/keymaps/dam5h/keymap.c
@@ -0,0 +1,69 @@
+/* Copyright 2022 James White <jamesmnw@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum layers {
+   _QWERTY,
+   _RS,
+   _LW,
+};
+
+
+const uint16_t PROGMEM esc_combo[] = {KC_D, KC_F, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(esc_combo, KC_ESC),
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/*
+ * QWERTY
+ * ,---------------------------------.     ,----------------------------------.
+ * |  Q  |   W  |   E  |   R  |   T  |     |   Y  |   U  |   I  |   O  |   P  |
+ * |-----+------+------+------+------|     |------+------+------+------+------|
+ * |  A  |   S  |   D  |   F  |   G  |     |   H  |   J  |   K  |   L  |   ;  |
+ * |-----+------+------+------+------|     ,------+------+------+------+------|
+ * |  Z  |   X  |   C  |   V  |   B  |     |   N  |   M  |   ,  |   .  |   /  |
+ * `------------+------+------+------|     |------+------+------+-------------'
+ *              | LCTL | BSpc | Esc  |     | Ent  | Spc  | LAlt |
+ *              `--------------------'     `--------------------'
+ */
+
+  [_QWERTY] = LAYOUT(
+    KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+    KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+    KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,                       KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
+
+    KC_ESC, KC_TAB, LGUI_T(KC_TAB), KC_LSFT, LCTL_T(KC_BSPC),  KC_NO, KC_NO,   LT(_RS, KC_SPC),  KC_ENT,  LALT_T(KC_MINS), KC_NO, KC_NO
+  ),
+  [_RS] = LAYOUT( /* [> RAISE <] */
+    KC_EXLM, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
+    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_QUOT, KC_4,    KC_5,   KC_6, KC_EQL ,
+    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
+
+    KC_NO, KC_NO, KC_LGUI, LSFT_T(TG(_LW)), KC_0,  KC_NO, KC_NO,   KC_SPC,  KC_TRNS,  KC_DOT, KC_NO, KC_NO
+  ),
+
+  [_LW] = LAYOUT( /* [> LOWER <] */
+    KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
+    KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
+    KC_NO,   KC_VOLU, KC_NO,   KC_NO,   QK_BOOT,                   KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
+    
+    KC_NO, KC_NO, KC_LGUI, KC_TRNS, KC_BSPC,    KC_NO, KC_NO,      KC_SPC,  DF(_QWERTY), KC_TRNS, KC_NO, KC_NO
+  )
+
+};
